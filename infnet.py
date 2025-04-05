802692d6300d468b501b1a467e9b54733a552821
LeticiaFAAGomes <leticia.gomes@al.infnet.edu.br>
2025-04-05 19:56:53 -0300

    adicao de algumas validacoes do formulario de alunos com js e conexao do formulario com banco de dados

--- infnet.py
+++ infnet.py
@@ -1,20 +1,43 @@
 from crud import *
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from starlette.staticfiles  import StaticFiles
 
 
-incluir_aluno('Letícia', 'Rua da Letícia', emails=['leticia@email.com', 'leticia@al.infnet.edu.br'])
-incluir_aluno('Arthur', 'Rua do Arthur', emails=['arthur@email.com'])
-incluir_aluno('Augusto', 'Rua do Augusto', emails=['augusto@email.com'])
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static") 
+
+
+@app.get("/")
+async def navegar_index(request:Request):
+    return templates.TemplateResponse("aluno.html", {"request": request})
+
+
+@app.post('/dados_aluno')
+async def carregar_dados(request:Request):
+    dados = await request.json()
+    nome_aluno = dados.get('nome')
+    endereco_aluno = dados.get('endereco')
+    emails_aluno = dados.get('emails')
+    incluir_aluno(nome=nome_aluno, endereco=endereco_aluno, emails=emails_aluno)
+    return {'mensagem':f'os dados de {nome_aluno} foram salvos com sucesso!'}
+
+
+# incluir_aluno('Letícia', 'Rua da Letícia', emails=['leticia@email.com', 'leticia@al.infnet.edu.br'])
+# incluir_aluno('Arthur', 'Rua do Arthur', emails=['arthur@email.com'])
+# incluir_aluno('Augusto', 'Rua do Augusto', emails=['augusto@email.com'])
 
 incluir_disciplinas('SQL', 400)
 incluir_disciplinas('Python', 400)
 incluir_disciplinas('Projeto de Bloco', 500)
 incluir_disciplinas('C++', 400)
 
-incluir_aluno_disciplina(1, 1)
-incluir_aluno_disciplina(1, 2)
-incluir_aluno_disciplina(1, 3)
-incluir_aluno_disciplina(2, 1)
-incluir_aluno_disciplina(2, 3)
+# incluir_aluno_disciplina(1, 1)
+# incluir_aluno_disciplina(1, 2)
+# incluir_aluno_disciplina(1, 3)
+# incluir_aluno_disciplina(2, 1)
+# incluir_aluno_disciplina(2, 3)
 
 consultar_alunos()
 excluir_aluno(1)
---
+++ static/js/cadastro_aluno.js
@@ -0,0 +1,78 @@
+function verificarCamposObrigatorios(dados) {
+  return dados.every((dado) => dado != "");
+}
+
+function addEmail(row1) {
+  const div = document.getElementById("emails");
+  const row = document.createElement("div");
+
+  row.classList.add("row");
+  row.innerHTML = ` <input type="email" class="email" placeholder="Insira o e-mail do aluno" />
+                    <a onclick="addEmail(this)" id="btn_email"><img src="/static/images/add.svg" alt="" /></a>`;
+  div.appendChild(row);
+  row1.remove();
+}
+
+function addLista(osEmails) {
+  const emails = [];
+  for (let i = 0; i < osEmails.length; i++) {
+    emails.push(osEmails[i].value);
+  }
+  return emails;
+}
+
+function exibirMsg(ehSucesso = false, msg) {
+  const aviso = document.getElementById("aviso");
+  if (ehSucesso) {
+    aviso.classList.add("sucesso");
+    aviso.innerHTML = `${msg}`;
+    setTimeout(() => {
+      aviso.classList.remove("sucesso");
+    }, 2000);
+  } else {
+    aviso.classList.add("erro");
+    aviso.innerHTML = `${msg}`;
+    setTimeout(() => {
+      aviso.classList.remove("erro");
+    }, 2000);
+  }
+}
+
+async function enviarDado(dados) {
+  try {
+    await fetch("/dados_aluno", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(dados),
+    });
+  } catch (erro) {
+    console.log("Não foi possível realizar o cadastro:", erro);
+  }
+}
+
+function registrarAluno(nome, endereco, emails) {
+  let validaCamposObrigatorios = verificarCamposObrigatorios([nome, endereco, emails]);
+  let listaEmails = addLista(emails);
+
+  if (validaCamposObrigatorios) {
+    enviarDado({ nome: nome, endereco: endereco, emails: listaEmails });
+    return {
+      ehSucesso: true,
+      msg: `O aluno ${nome} foi cadastrado com sucesso.`,
+    };
+  } else {
+    return {
+      ehSucesso: false,
+      msg: "Todos os campos devem ser preenchidos!",
+    };
+  }
+}
+
+function cadastrar() {
+  const nome = document.getElementById("nome").value;
+  const endereco = document.getElementById("endereco").value;
+  const emails = document.getElementsByClassName("email");
+
+  msg = registrarAluno(nome, endereco, emails);
+  exibirMsg(msg.ehSucesso, msg.msg);
+}
